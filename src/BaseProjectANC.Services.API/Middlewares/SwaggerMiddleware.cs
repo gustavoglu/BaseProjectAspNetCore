275ac0e42dbbbc01530c978fa45de43aa7788917
Gustavo Giroldo <gustavoglu@hotmail.com>
2017-07-30 11:24:14 -0300

    Trabalhando na camada de Jwt da API e Identity, adicionado swagger

---
+++ src/BaseProjectANC.Services.API/Middlewares/SwaggerMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using BaseProjectANC.Domain.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseProjectANC.Services.API.Middlewares
+{
+    public class SwaggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private IUser _user;
+
+        public SwaggerMiddleware(RequestDelegate next, IUser user)
+        {
+            _next = next;
+            _user = user;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/swagger") && !_user.IsAuthenticated())
+            {
+
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                return;
+            };
+
+            await _next.Invoke(context);
+        }
+    }
+    public static class SwaggerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSWaggerAuthorized(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SwaggerMiddleware>();
+        }
+    }
+}
--- src/BaseProjectANC.Services.API/Startup.cs
+++ src/BaseProjectANC.Services.API/Startup.cs
@@ -15,6 +15,18 @@ using Microsoft.EntityFrameworkCore;
 using BaseProjectANC.Infra.Identity.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using BaseProjectANC.Services.API.Configurations;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Swagger;
+using BaseProjectANC.Services.API.Middlewares;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using BaseProjectANC.Infra.Identity.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace BaseProjectANC.Services.API
 {
@@ -31,10 +43,13 @@ namespace BaseProjectANC.Services.API
         }
 
         public IConfigurationRoot Configuration { get; }
-
+        private const string SecretKey = "BaseProjectAspNetCoreSercretKey";
+        private readonly SymmetricSecurityKey _signKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey)); 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+         
 
             services.AddDbContext<ApplicationDbContext>(opt =>
             opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnectoin")));
@@ -43,9 +58,45 @@ namespace BaseProjectANC.Services.API
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddMvc();
+            services.AddOptions();
+            services.AddMvc(options =>
+            {
+                options.OutputFormatters.Remove(new XmlDataContractSerializerOutputFormatter());
+                //  options.UseCentralRoutePrefix(new RouteAttribute("api/v{version}"));
+                var policy = new AuthorizationPolicyBuilder()
+                .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser()
+                .Build();
+
+                options.Filters.Add(new AuthorizeFilter(policy));
+
+            });
+
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtTokenOptions));
+
+            services.Configure<JwtTokenOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtTokenOptions.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(JwtTokenOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signKey, SecurityAlgorithms.HmacSha256);
+            });
 
             services.AddAutoMapper();
+
+
+            services.AddSwaggerGen(s => {
+
+                s.SwaggerDoc("v1", new Info()
+                {
+                    Version = "v1",
+                    Title = "BaseProjectANC API",
+                    Description = "API do Projeto Base de Asp Net Core",
+                    TermsOfService = "Nenhum",
+                    Contact = new Contact { Name = "Gustavo Giroldo", Email = "gustavoglu@hotmail.com" },
+                });
+
+            });
+
 
             NativeSimpleInjector.RegisterDependencys(services);
 
@@ -57,7 +108,36 @@ namespace BaseProjectANC.Services.API
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtTokenOptions));
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtTokenOptions.Audience)],
+
+                ValidateAudience = true,
+                ValidAudience = jwtAppSettingOptions[nameof(JwtTokenOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signKey,
+
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+
+                ClockSkew = TimeSpan.Zero
+
+            };
+
+            app.UseStaticFiles();
+            app.UseIdentity();
             app.UseMvc();
+
+            //app.UseSWaggerAuthorized();
+            app.UseSwagger();
+            app.UseSwaggerUI(s => 
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "BaseProjectANC API V1");
+            });
 
             InMemoryBus.ContainerAccessor = () => accessor.HttpContext.RequestServices;
         }
